beeea0ddf36607001be807d74e5d4767e19ceb5a
MichaelMtz <michaelmtz@mtzdesigns.com>
2021-09-13 16:35:49 -0600

    Webpack asset managment examples completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
+import "./styles/styles.css";
+// import Icon from './images/info.png';
 
  function component() {
    const element = document.createElement('div');
 
   // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'Webby Pack'], ' ');
+   element.classList.add('hello');
 
+  //  const elIcon = new Image();
+  //  elIcon.src = Icon;
+  //  element.appendChild(elIcon);
    return element;
  }
 
